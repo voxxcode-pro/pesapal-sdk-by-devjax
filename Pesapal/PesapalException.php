647bebe39419f673c7f8fec0e0e326d04ed35766
Marick Marick <120605354+voxxcode-pro@users.noreply.github.com>
2025-08-16 01:35:22 +0300

    Create PesapalException.php

---
+++ Pesapal/PesapalException.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace DevJax\Pesapal;
+
+/**
+ * Custom Pesapal Exception
+ *
+ * @file     Pesapal/PesapalException.php
+ * @author   DevJax
+ */
+class PesapalException extends \Exception {
+    // Custom exception for the SDK to provide more specific error details.
+}
